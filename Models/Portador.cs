609498503788f193ec4b7e136e91da2d34786d68
Humberto <jairhumberto@gmail.com>
2022-02-14 21:32:17 -0300

    Inclusao da model portador

---
+++ Models/Portador.cs
@@ -0,0 +1,9 @@
+namespace PortadoresService.Models
+{
+    public class Portador
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+    }
+}
