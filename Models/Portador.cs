11b98ec5ff2a82a670eddcb9350a039b9df54793
Humberto <jairhumberto@gmail.com>
2022-02-14 21:33:29 -0300

    Adicao das annotations para validacao dos dados

--- Models/Portador.cs
+++ Models/Portador.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PortadoresService.Models
 {
     public class Portador
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        
+        [Required]
         public string Nome { get; set; }
+        
+        [Required]
         public string Cpf { get; set; }
     }
 }
